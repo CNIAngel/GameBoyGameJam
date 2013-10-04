6d74870b675f5abb666953a5b3e4f6b340ed0fb4
Grant <wolfenangel8@gmail.com>
2013-10-03 23:20:26 -0400

    Added two entity classes
    I've added the classes for the final objects in this game and will begin
    integrating them tomorrow.

---
+++ gb/src/com/cnia/gb/entity/LaserBase.java
@@ -0,0 +1,97 @@
+package com.cnia.gb.entity;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class LaserBase implements Entity {
+
+	public LaserBase() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(SpriteBatch b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public float getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Vector2 getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setX(float newX) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setY(float newY) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setWidth(float newWidth) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHeight(float newHeight) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPosition(Vector2 newPosition) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setBounds(Rectangle newBounds) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
